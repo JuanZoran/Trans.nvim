5cf91a5a17d777410a6952e29accedb22bd7f055
JuanZoran <1430359574@qq.com>
2022-12-17 20:51:22 +0800

    feat: define some feature todo

--- lua/Trans/init.lua
+++ lua/Trans/init.lua
@@ -1,20 +1,61 @@
-M = {}
+local M = {}
 
-local default_config = {
+
+-- NOTE:
+-- Style:
+--      [minimal]: one line with '/'
+--      [default]:
+--      [full]:    show all description in different lines
+-- TODO: other style
+
+
+local default_conf =
+{
     display = {
-        style = '',
+        style = 'default',
         phnoetic = true,
         collins_star = true,
         tag = true,
         oxford = true,
+        -- TODO: frequency
+        -- frequency = false,
+        history = false,
     },
-    default_cmd = true,
+    default_keymap = true,
+    map = {
+
+    },
+    
+    view = {
+        -- TODO: style: buffer | cursor | window
+        -- style = 'buffer',
+        -- buffer_pos = 'bottom', -- only works when view.style == 'buffer'
+    },
+
+    db_path = '/home/zoran/project/neovim/ecdict-sqlite-28/stardict.db', -- FIXME: change the path
+
+    -- TODO: async process
+    async = false,
+
+    -- TODO:  add online translate engine
+    -- online_search = {
+    --     enable = false,
+    --     engine = {},
+    -- }
+
+    -- TODO: precise match or return closest match result
+    -- precise_match = true,
+
+    -- TODO: leamma search
+    -- leamma = false,
+
+    -- TODO: register word
 }
 
-M.config = default_config
+M.conf = default_conf
 
-function M.setup(conf)
-    M.config = vim.tbl_extend('force', default_config, conf)
+function M:setup(conf)
+    self.config = vim.tbl_extend('force', default_conf, conf)
 end
 
 return M
