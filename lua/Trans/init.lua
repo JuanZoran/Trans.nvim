ce688eb670783b7d1963ab20e51c94c362392a1a
JuanZoran <1430359574@qq.com>
2023-01-26 23:55:19 +0800

    fix: fix window open and neovim version

--- lua/Trans/init.lua
+++ lua/Trans/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 
-local title = vim.fn.has('nvim-0.9') and {
+local title = vim.fn.has('nvim-0.9') == 1 and {
     { '', 'TransTitleRound' },
     { ' Trans', 'TransTitle' },
     { '', 'TransTitleRound' },
