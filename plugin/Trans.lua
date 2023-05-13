871cc1e48a2c1ddc8c5aa14946263ed283ad425f
xiaomo <m19357513004@163.com>
2023-05-13 18:13:32 +0800

    refactor: wsl

--- plugin/Trans.lua
+++ plugin/Trans.lua
@@ -24,8 +24,7 @@ string.width = api.nvim_strwidth
 
 local system = Trans.system
 local f =
-    vim.fn.has 'wsl' == 1 and 'powershell.exe -Command "Add-Type -AssemblyName System.speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\\\"%s\\\")"' or
-    system == 'win' and 'powershell -Command "Add-Type -AssemblyName System.speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\\\"%s\\\")"' or
+    (vim.fn.has 'wsl' or system == 'win') and 'powershell.exe -Command "Add-Type -AssemblyName System.speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\\\"%s\\\")"' or
     system == 'mac' and 'say %q' or
     system == 'termux' and 'termux-tts-speak %q' or
     system == 'linux' and 'echo %q | festival --tts'
